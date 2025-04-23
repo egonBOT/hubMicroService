6b7cd16792f92796bca4855d1f7ed1d3dbf38a6c
EgonBot <94461017+egonBOT@users.noreply.github.com>
2025-04-23 09:53:35 -0300

    Add ms-Pedido

---
+++ microJavaPedido/src/main/java/com/github/EgonBot/microJavaPedido/MicroJavaPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.github.EgonBot.microJavaPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroJavaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroJavaPedidoApplication.class, args);
+	}
+
+}
---
+++ microJavaPedido/src/test/java/com/github/EgonBot/microJavaPedido/MicroJavaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.EgonBot.microJavaPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroJavaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
